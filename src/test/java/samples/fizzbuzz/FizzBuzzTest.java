35be53d7f604c1c08f57d165242280bf2f731afd
Yoel Lorenzo <yoel.lore@gmail.com>
2021-06-09 19:28:46 -0400

    Adding a test for the number 1 and getting red

--- src/test/java/samples/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/samples/fizzbuzz/FizzBuzzTest.java
@@ -1,10 +1,12 @@
 package samples.fizzbuzz;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class FizzBuzzTest {
     @Test
     public void testNumbers() {
         FizzBuzz fizzBuzz = new FizzBuzz();
+        Assert.assertEquals("1", fizzBuzz.convert(1));
     }
 }
