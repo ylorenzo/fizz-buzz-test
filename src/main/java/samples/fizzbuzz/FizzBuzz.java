b7e73ea123b7eba9148b74cafcd981bf53eb3652
Yoel Lorenzo <yoel.lore@gmail.com>
2021-06-09 19:54:26 -0400

    Updating the convert method to return Buzz and getting green again

--- src/main/java/samples/fizzbuzz/FizzBuzz.java
+++ src/main/java/samples/fizzbuzz/FizzBuzz.java
@@ -4,6 +4,8 @@ public class FizzBuzz {
     public String convert(int number) {
         if (number % 3 == 0){
             return "Fizz";
+        } else if (number % 5 == 0) {
+            return "Buzz";
         } else {
             return String.valueOf(number);
         }
