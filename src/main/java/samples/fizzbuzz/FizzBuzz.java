61bc4d54ca21e7fdc2240c1e09b3a4e201462025
Yoel Lorenzo <yoel.lore@gmail.com>
2021-06-09 19:41:18 -0400

    Updating the convert method to return Fizz and getting green again

--- src/main/java/samples/fizzbuzz/FizzBuzz.java
+++ src/main/java/samples/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,10 @@ package samples.fizzbuzz;
 
 public class FizzBuzz {
     public String convert(int number) {
-        return String.valueOf(number);
+        if (number % 3 == 0){
+            return "Fizz";
+        } else {
+            return String.valueOf(number);
+        }
     }
 }
