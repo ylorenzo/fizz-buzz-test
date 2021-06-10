fccb12444c277e39eaf13cabae1b48ca1687a59c
Yoel Lorenzo <yoel.lore@gmail.com>
2021-06-09 20:25:35 -0400

    Adding a main method to print the numbers

--- src/main/java/samples/fizzbuzz/FizzBuzz.java
+++ src/main/java/samples/fizzbuzz/FizzBuzz.java
@@ -1,6 +1,14 @@
 package samples.fizzbuzz;
 
 public class FizzBuzz {
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(fizzBuzz.convert(i));
+        }
+    }
+
     public String convert(int number) {
         if (number % 3 == 0 && number % 5 == 0) {
             return "FizzBuzz";
